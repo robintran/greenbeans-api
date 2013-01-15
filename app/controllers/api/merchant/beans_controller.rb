0dd79218884aee41fa5af36e0ef27dc0c0a064ab
Bhanu Prasad <bhanuprasad143@gmail.com>
2013-01-15 11:17:40 +0700

    action validate code for beans controller

--- app/controllers/api/merchant/beans_controller.rb
+++ app/controllers/api/merchant/beans_controller.rb
@@ -1,2 +1,18 @@
 class Api::Merchant::BeansController < Api::Merchant::BaseController
+
+  def validate
+    render json: {valid: false, message: "param code cannot be blank"} and return if params[:code].blank?
+    bean = Bean.find_by_code params[:code]
+
+    if bean
+      if bean.used_on == Bean::USED_ON[:none]
+        render json: {valid: true, message: "#{bean.code} is valid"}
+      else
+        render json: {valid: false, message: "#{bean.code} is invalid, used on #{bean.used_on}"}
+      end
+    else
+      render json: {valid: false, message: "#{params[:code]} not found"}
+    end
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,32 +2,37 @@ Greenbean::Application.routes.draw do
   devise_for :merchants, :users
 
   namespace :api do
-    get 'docs/index' 
+    get 'docs/index'
     match 'docs' => 'docs#index'
-     
+
     namespace :merchant do
-      resources :beans
-      resources :tokens, :only => [:create] do 
-        collection do 
+      resources :beans do
+        collection do
+          get :validate
+        end
+      end
+      resources :tokens, :only => [:create] do
+        collection do
           get :beans
         end
       end
-      
-      resources :sessions, :only  => [:create] do 
-        collection do 
+
+      resources :sessions, :only  => [:create] do
+        collection do
           post :delete
         end
       end
     end
-    
+
     namespace :consumer do
-      resources :sessions, :only  => [:create] do 
-        collection do 
+      resources :sessions, :only  => [:create] do
+        collection do
           post :delete
         end
       end
     end
   end
-  
+
   root :to => 'api/docs#index'
 end
+
