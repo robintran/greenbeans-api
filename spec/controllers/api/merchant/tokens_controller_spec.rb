00073c0b0fb67326ff55a6aa21edf0212cf4d9a6
Bhanu Prasad <bhanuprasad143@gmail.com>
2013-01-16 16:44:15 +0700

    test tokens_controller

--- spec/controllers/api/merchant/tokens_controller_spec.rb
+++ spec/controllers/api/merchant/tokens_controller_spec.rb
@@ -2,4 +2,26 @@ require 'spec_helper'
 
 describe Api::Merchant::TokensController do
 
+  let!(:merchant) { FactoryGirl.create(:merchant) }
+  
+  before do
+    sign_in merchant  
+  end
+  
+  describe "Create" do
+    it "should return error when user not signed in yet" do
+      sign_out merchant
+      post :create, format: :json
+      response.should_not be_success
+      data = JSON.parse(response.body)
+      data["error"].should eq("You need to sign in or sign up before continuing.")
+    end
+    
+    it "should not create token without quantity" do
+      post :create, format: :json
+      response.should be_success
+      data = JSON.parse(response.body)
+    end
+  end
+  
 end
