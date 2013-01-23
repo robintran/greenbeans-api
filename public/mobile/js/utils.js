f5e8c0a5efb0ca8c82fcfd0fa5c3a8ff88f4b887
Srikanth Kanthi <srikanth.kanthi143@gmail.com>
2013-01-23 17:54:15 +0700

    Update mobile web app

--- public/mobile/js/utils.js
+++ public/mobile/js/utils.js
@@ -1,3 +1,4 @@
+var gb_auth_token;
 function claimBeans (beanCode) {
     var params = {
         
@@ -27,18 +28,37 @@ function signin (mail, pass) {
         params, function(response) {
             $.mobile.hidePageLoadingMsg();
             if (response.status == 200) {
+                console.log(response.merchant.auth_token);
                 window.localStorage.setItem("gb_auth_token", response.merchant.auth_token);
+                console.log(window.localStorage.getItem("gb_auth_token"));
+                gb_auth_token = response.merchant.auth_token;
                 $.mobile.changePage( "homeBeans.html", { transition: "slide"} );
+                loadMyBeans();
             }
             console.log(response);
         }
     )
 }
 
+function loadMyBeans () {
+    // var params = {
+    // }
+    // $.mobile.showPageLoadingMsg();
+    // var url = 'http://107.20.196.96/api/consumer/beans/my_beans.json?auth_token=' + gb_auth_token;
+    // $.get(url,
+    //     params, function(response) {
+    //         $.mobile.hidePageLoadingMsg();
+    //         console.log(response);
+    //     }
+    // )
+}
+
 function getMyBeans() {
     var auth_token = window.localStorage.getItem("gb_auth_token");
     if (auth_token && auth_token.length > 0) {
         $.mobile.changePage( "homeBeans.html", { transition: "slide"} );
+        gb_auth_token = auth_token;
+        loadMyBeans();
     } else {
         $.mobile.changePage( "signup.html", { transition: "slide"} );
     }
