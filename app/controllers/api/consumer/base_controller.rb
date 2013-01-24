21de481e7e1f6f7232db1ec7da5b1715c0acb33c
Bhanu Prasad <bhanuprasad143@gmail.com>
2013-01-24 19:00:28 +0700

    add CORS for api call

--- app/controllers/api/consumer/base_controller.rb
+++ app/controllers/api/consumer/base_controller.rb
@@ -1,9 +1,19 @@
 class Api::Consumer::BaseController < ApplicationController
   skip_before_filter :verify_authenticity_token
-  before_filter  :authenticate_user!
+  before_filter  :authenticate_user!, :set_headers
 
   respond_to :json
 
+  def set_headers
+    if request.headers["HTTP_ORIGIN"] #&& /^https?:\/\/(.*)\.some\.site\.com$/i.match(request.headers["HTTP_ORIGIN"])
+      headers['Access-Control-Allow-Origin'] = request.headers["HTTP_ORIGIN"]
+      headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE'
+      headers['Access-Control-Allow-Headers'] = '*,x-requested-with,Content-Type,If-Modified-Since,If-None-Match,Auth-User-Token'
+      headers['Access-Control-Max-Age'] = '86400'
+      headers['Access-Control-Allow-Credentials'] = 'true'
+    end
+  end
+  
   rescue_from Exception do |exception|
     render :json => {status: 500, auth: false, message: exception.message}
   end
--- app/controllers/api/merchant/base_controller.rb
+++ app/controllers/api/merchant/base_controller.rb
@@ -1,9 +1,23 @@
 class Api::Merchant::BaseController < ApplicationController
   skip_before_filter :verify_authenticity_token
-  before_filter  :authenticate_merchant!
-
+  before_filter  :authenticate_merchant!, :set_headers
+  
   respond_to :json
 
+  before_filter :set_headers
+
+  private
+
+  def set_headers
+    if request.headers["HTTP_ORIGIN"] #&& /^https?:\/\/(.*)\.some\.site\.com$/i.match(request.headers["HTTP_ORIGIN"])
+      headers['Access-Control-Allow-Origin'] = request.headers["HTTP_ORIGIN"]
+      headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE'
+      headers['Access-Control-Allow-Headers'] = '*,x-requested-with,Content-Type,If-Modified-Since,If-None-Match,Auth-User-Token'
+      headers['Access-Control-Max-Age'] = '86400'
+      headers['Access-Control-Allow-Credentials'] = 'true'
+    end
+  end
+  
   rescue_from Exception do |exception|
     output = generate_exception_output(exception)
     render :json => output
